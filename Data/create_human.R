5f83aee6337c453b891661297eba933b5d905a45
Suvi Kivi <suvi.kivi@vtv.fi>
2018-12-02 23:12:04 +0200

    Chapter5

--- Data/create_human.R
+++ Data/create_human.R
@@ -1,3 +1,5 @@
+## EXERCISE FOR WEEK 4 ##
+
 # 2.Read the datasets in 
 
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -45,4 +47,53 @@ human <- merge(gii, hd, by="Country")
 # There is 195 obs and 19 variables in the human data.
 
 write.csv(human, "/Users/suvi/Documents/GitHub/IODS-project/Data/human.csv")
+
+
+#########################################
+
+## EXERCISE FOR WEEK 5 ##
+
+str(human)
+dim(human)
+summary(human)
+
+# 1. Transform data to numeric
+
+library(stringr)
+str(human$GNI)
+human$GNI <- str_replace(human$GNI, pattern=",", replace="") %>% as.numeric
+str(human$GNI)
+
+# 2. Exclude unneeded variables
+
+keep <- c("Country", "EduRatio", "Labratio", "EduExp", "LifeExp", "GNI", "MMRatio", "ABirthR", "RepPar")
+human <- select(human, one_of(keep))
+
+# 3. Remove missing values
+
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human2 <- filter(human, complete.cases(human))
+
+# 4. Remove observations which relate to regions
+
+str(human2$Country)
+#I'll remove observations Arab states (row 4), East Asia and the Pacific (row 44), Europe and Central Asia (row 50), 
+# Latin America and the Caribbean (row 81), South Asia (row 132), Sub-Saharan Africa (row 135), World (159)
+
+human3 <- (human2[-c(4, 44, 50, 81, 132, 135, 159),])
+
+
+# 5. Define row names and remove the country name column
+
+rownames(human3) <- human3$Country
+keep2 <- c("EduRatio", "Labratio", "EduExp", "LifeExp", "GNI", "MMRatio", "ABirthR", "RepPar")
+human3 <- select(human3, one_of(keep2))
+
+# Data now includes 155 obs and 8 variables.
+
+write.csv(human3, "/Users/suvi/Documents/GitHub/IODS-project/Data/human3.csv", row.names = TRUE)
+
+
+
 
